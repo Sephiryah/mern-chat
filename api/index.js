a1fc5b2fd3eee2b91e38871b4d0f0f69bfd8896e
Sephirah <nicolas.bussiere@outlook.fr>
2023-03-10 14:01:50 +0100

    Add backend for user creation

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+const User = require('./models/User')
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+
+const app = express();
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+})
+
+app.post('/register', async (req, res) => {
+    const {username,password} = req.body;
+    await User.create({username,password});
+    res.json();
+})
+
+app.listen(4000)
